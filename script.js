4b171892457f42f297ca7a399664a85f1c0e313c
sasa202020 <sosiso4ki64@gmail.com>
2022-09-11 19:35:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/* ======================================================================
+  Author Custom JavaScript
+====================================================================== */
+// Loop through Array of Objects
+var objPeople = [
+  {
+    // Object @ 0 index
+    username: "Alex",
+    password: "123",
+  },
+  {
+    // Object @ 1 index
+    username: "Mads",
+    password: "123",
+  },
+  {
+    // Object @ 2 index
+    username: "123",
+    password: "123",
+  },
+];
+
+function getInfo() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  for (var i = 0; i < objPeople.length; i++) {
+    // check is user input matches username and password of a current index of the objPeople array
+
+    if (username == objPeople[i].username && password == objPeople[i].password) {
+      window.location.href = "http://stackoverflow.com";
+      // stop the function if this is found to be true
+      return;
+    }
+  }
+  console.log("incorrect username or password");
+}
