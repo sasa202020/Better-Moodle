c76386547c7092fb947d63dc9590a7febb6400b7
LaerkeSoeby <laer9384@edu.zealand.dk>
2022-10-21 11:36:43 +0200

    Deleted unused css and html

--- structure/script(carousel).js
+++ structure/script(carousel).js
@@ -1,22 +1,3 @@
-/*const buttons = document.querySelectorAll("[data-carousel-button]")
-
-buttons.forEach(button => {
-   button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
-
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
-
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active 
-   })
-})*/
-
-// Inspo from https://www.youtube.com/watch?v=9HcxHDS2w1s 
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -41,3 +22,5 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
   
 }
+
+// inspo from https://www.w3schools.com/howto/howto_js_slideshow.asp 
