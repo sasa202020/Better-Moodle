46e7166cf7fcec75dad817dba8d2ca8209444bcc
sasa202020 <sosiso4ki64@gmail.com>
2022-10-24 21:30:17 +0200

    online release

--- structure/script(old+bordersOnLogin).js
+++ structure/script(old+bordersOnLogin).js
@@ -19,32 +19,6 @@ var objPeople = [{
     },
 ];
 
-function getInfo() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    for (var i = 0; i < objPeople.length; i++) {
-        // check is user input matches username and password of a current index of the objPeople array
-
-        if (username == "Mads" && password == "123") {
-            window.location.href = "http://stackoverflow.com";
-            // stop the function if this is found to be true
-            return;
-        }
-        if (username == "Alex" && password == "123") {
-            window.location.href = "http://youtube.com";
-            // stop the function if this is found to be true
-            return;
-        }
-        if (username == "123" && password == "123") {
-            window.location.href = "structure/index.html";
-            // stop the function if this is found to be true
-            return;
-        }
-    }
-    console.log("incorrect username or password");
-}
-
 // inspiration from https://www.geeksforgeeks.org/how-to-change-input-box-borders-after-filling-the-box-using-javascript/ 
 var borderGreenEmail = document.getElementById("username");
 
